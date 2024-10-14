6cab28218cc2f4810564c9513bfab4f22bc17b22
pmpr.dev <pmpr.dev@gmail.com>
2024-10-14 23:51:08 +0000

    Obfuscated

--- src/Pmpr/Cover/Gfan/Container.php
+++ src/Pmpr/Cover/Gfan/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d4a8d36f94             |
+    |             670dae380e6d9             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Gfan; use Pmpr\Common\Foundation\Container\Container as BaseClass; abstract class Container extends BaseClass { }
--- src/Pmpr/Cover/Gfan/Widget/Link.php
+++ src/Pmpr/Cover/Gfan/Widget/Link.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d4a8d36f94             |
+    |             670dae380e6d9             |
     |_______________________________________|
 */
- namespace Pmpr\Cover\Gfan\Widget; use Pmpr\Common\Foundation\Interfaces\Constants; use Pmpr\Common\Foundation\Widget; class Link extends Widget { public function __construct() { parent::__construct(__("\x4c\151\x6e\x6b\163", PR__CVR__GFAN), __("\104\x69\163\160\x6c\141\171\x20\x74\x68\145\40\x73\145\154\x65\143\x74\x65\144\x20\x6c\151\x6e\153\163\x2e", PR__CVR__GFAN)); } public function ykwqaukkycogooii() { $uuyucgkyusckoaeq = $this->caokeucsksukesyo()->wmkogisswkckmeua(); $this->mkksewyosgeumwsa($uuyucgkyusckoaeq->mccagaqeagiikkec(Constants::qwumqqyuasyskkkc)->gswweykyogmsyawy(__("\111\164\145\x6d\x73", PR__CVR__GFAN))->ukqywcsoogkyoaoa()); } }
+ namespace Pmpr\Cover\Gfan\Widget; use Pmpr\Common\Foundation\Interfaces\Constants; use Pmpr\Common\Foundation\Widget; class Link extends Widget { public function __construct() { parent::__construct(__("\x4c\x69\156\x6b\x73", PR__CVR__GFAN), __("\x44\151\163\160\154\141\171\x20\x74\150\x65\40\x73\145\154\x65\x63\x74\x65\144\40\x6c\151\x6e\x6b\x73\x2e", PR__CVR__GFAN)); } public function ykwqaukkycogooii() { $uuyucgkyusckoaeq = $this->caokeucsksukesyo()->wmkogisswkckmeua(); $this->mkksewyosgeumwsa($uuyucgkyusckoaeq->mccagaqeagiikkec(Constants::qwumqqyuasyskkkc)->gswweykyogmsyawy(__("\111\x74\x65\155\163", PR__CVR__GFAN))->ukqywcsoogkyoaoa()); } }
