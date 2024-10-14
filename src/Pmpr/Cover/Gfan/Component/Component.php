1ea70fe98dba236ed0509a443cc6bbfc4c6d6271
pmpr.dev <pmpr.dev@gmail.com>
2024-10-14 23:52:43 +0000

    Obfuscated

--- src/Pmpr/Cover/Gfan/Component/Component.php
+++ src/Pmpr/Cover/Gfan/Component/Component.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670dae380e6d9             |
+    |             670dae97375ea             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Gfan\Component; use Pmpr\Cover\Gfan\Component\Module\Contact; use Pmpr\Cover\Gfan\Container; class Component extends Container { public function mameiwsayuyquoeq() { Contact::symcgieuakksimmu(); } }
--- src/Pmpr/Cover/Gfan/Container.php
+++ src/Pmpr/Cover/Gfan/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670dae380e6d9             |
+    |             670dae97375ea             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Gfan; use Pmpr\Common\Foundation\Container\Container as BaseClass; abstract class Container extends BaseClass { }
